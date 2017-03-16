e463614f6fc4682ef6d1e3f32984187cedf592b7
Michał Klawitter <mikesrike@imac.speedportentry2090126200030>
2017-03-16 13:20:05 +0100

    heroku hotfix

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => 'https://boiling-everglades-90622.herokuapp.com/',
 
     /*
     |--------------------------------------------------------------------------
